2c1c83c55c51385383742313b69d5d9609a0afbc
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-12-15 06:29:38 -0500

    Cleaned up schema.sql and seed.sql. Moved actual DDLs into comments.

--- src/main/resources/database/seed.sql
+++ src/main/resources/database/seed.sql
@@ -1,11 +1,15 @@
-INSERT INTO employees (name,email) VALUES ('John Doe', 'john@email.com');
-INSERT INTO employees (name,email) VALUES ('Jane Doe', 'jane@email.com');
-INSERT INTO employees (name,email) VALUES ('Jack Doe', 'jack@email.com');
-INSERT INTO employees (name,email) VALUES ('Jill Doe', 'jill@email.com');
-INSERT INTO employees (name,email) VALUES ('Jenn Doe', 'jenn@email.com');
-
-INSERT INTO phonebook (name,number,note) VALUES ('John Doe', '415-555-1111', 'Home');
-INSERT INTO phonebook (name,number,note) VALUES ('John Doe', '415-555-2222', 'Cell');
-INSERT INTO phonebook (name,number,note) VALUES ('Jane Doe', '415-555-3333', 'Home');
-INSERT INTO phonebook (name,number,note) VALUES ('Jane Doe', '415-555-4444', 'Work');
-INSERT INTO phonebook (name,number,note) VALUES ('Jane Doe', '415-555-5555', 'Fax');
+--
+-- seed.sql
+--
+-- THIS IS A COMMENT.
+--
+-- DEFINE YOUR SEED DATA HERE (INSERT DDLs)
+--
+-- e.g.
+--
+-- INSERT INTO users (email,firstname,lastname,organization,title,password) VALUES ('john@email.com','John','Doe','My Organization','CFO','password');
+-- INSERT INTO users (email,firstname,lastname,organization,title,password) VALUES ('jane@email.com','Jane','Doe','My Organization','CEO','password');
+-- INSERT INTO users (email,firstname,lastname,organization,title,password) VALUES ('jack@email.com','Jack','Doe','My Organization','CIO','password');
+-- INSERT INTO users (email,firstname,lastname,organization,title,password) VALUES ('jill@email.com','Jill','Doe','Other Organization','CEO','password');
+-- INSERT INTO users (email,firstname,lastname,organization,title,password) VALUES ('jenn@email.com','Jenn','Doe','Other Organization','CIO','password');
+--
